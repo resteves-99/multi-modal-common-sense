80aba02ce22afeccfc0f4bc43c5ac5b90beb987e
resteves-99 <rafaele675@gmail.com>
2021-11-09 19:48:57 -0800

    fixed run file

--- run.py
+++ run.py
@@ -5,12 +5,13 @@ import argparse
 def run(args):
     train_processor = batch_processor(args.dataset, "train", attributes=args.attributes)
     train_labels, _, train_sentences, _ = train_processor.forward()
+    # print(train_sentences)
 
     test_processor = batch_processor(args.dataset, "test", attributes=args.attributes)
     test_labels, _, test_sentences, _ = test_processor.forward()
 
     accuracy = run_model(args.model, train_labels, train_sentences, test_labels, test_sentences)
-    print("The model achieved {accuracy}\% accuracy")
+    print(f"The model achieved {accuracy}% accuracy")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser('Flip some bits!')
