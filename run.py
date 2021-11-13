19e4ee05a3cd88ffcbdf0daf35055c267e31310e
resteves-99 <rafaele675@gmail.com>
2021-11-13 12:27:27 -0800

    prepared experiments

--- run.py
+++ run.py
@@ -4,24 +4,25 @@ import argparse
 import os
 
 def run(args):
+    # train data
     train_processor = batch_processor(args.dataset, "train", attributes=args.attributes)
     train_labels, _, train_sentences, _ = train_processor.forward()
     # print(train_sentences)
 
+    # test data
     test_processor = batch_processor(args.dataset, "test", attributes=args.attributes)
     test_labels, _, test_sentences, _ = test_processor.forward()
 
-    accuracy, error_idxs = run_model(args.model, train_labels, train_sentences, test_labels, test_sentences)
+    # run model
+    accuracy, error_idxs = run_model(args.model, train_labels, train_sentences, test_labels, test_sentences, try_encoder=args.try_encoder, verbose=args.verbose)
     print(f"The model achieved {accuracy}% accuracy")
 
+    # logging
     curr_directory = os.path.dirname(__file__)
-    print("daslkfasd", curr_directory)
-    extensions = ["./log", f"/{args.model}", f"/{args.dataset}", f"/{args.attributes}"]
+    extensions = ["./log", f"/{args.model}", f"/{args.dataset}", f"/{args.attributes}", f"/encoder_{args.try_encoder}"]
     for curr_dir in extensions:
         curr_directory = curr_directory + curr_dir
-        print('a', curr_directory, os.path.isdir(curr_directory))
         if not os.path.isdir(curr_directory):
-            print('b')
             os.mkdir(curr_directory)
     file = open(curr_directory + "/log.txt", "w")
     file.write(f"The model achieved {accuracy}% accuracy\n")
@@ -34,6 +35,8 @@ if __name__ == "__main__":
     parser.add_argument('--model', type=str, default="roberta",
                         help='')
     parser.add_argument('--dataset', type=str, default="verb")
+    parser.add_argument('--try_encoder', type=bool, default=True)
+    parser.add_argument('--verbose', type=bool, default=False)
 
     args = parser.parse_args()
     run(args)
--- script.py
+++ script.py
@@ -1,8 +1,25 @@
 import subprocess
 
 if __name__ == '__main__':
-    all_models = ["roberta", "clip"]
+    all_models = ['roberta', 'roberta_small', 'visualbert', 'uniter', 'clip']
+    all_datasets = ["doq", "verb"] #, "prost"]
+    dataset_attributes = {"verb": ['size', 'weight', 'strength', 'rigidness', 'speed'],
+                          "prost":['all', 'bouncing', 'breaking', 'circumference', 'directions', 'grasping',
+                                   'height', 'mass', 'rolling', 'sliding', 'stacking'],
+                          "doq": ["NA"]}
     for model in all_models:
-        print(f"{model} model!!!!!!!!!!!!")
-        command = ["python", "run.py", "--dataset", "doq", "--model", f"{model}"]
-        subprocess.run(command)
+        for dataset in all_datasets:
+            for attr in dataset_attributes[dataset]:
+                print(f"{model} model on {dataset} dataset with attribute {attr} with encodings!!!!!!!!!!!!")
+                command = ["python", "run.py", "--dataset", f"{dataset}", "--model", f"{model}", "--try_encoder", "True"]
+                subprocess.run(command)
+
+
+    all_models = ['roberta', 'roberta_small', 'visualbert', 'uniter', 'clip', 't5', 'lxmert']
+    all_datasets = ["doq", "verb", "prost"]
+    for model in all_models:
+        for dataset in all_datasets:
+            for attr in dataset_attributes[dataset]:
+                print(f"{model} model on {dataset} dataset with attribute {attr} with embeddings!!!!!!!!!!!!")
+                command = ["python", "run.py", "--dataset", f"{dataset}", "--model", f"{model}", "--try_encoder", "False"]
+                subprocess.run(command)
