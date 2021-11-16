5b2692599d0666c2aa923b5a3536bd9b643d3f77
resteves-99 <rafaele675@gmail.com>
2021-11-16 13:51:22 -0800

    alternative script

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 if __name__ == '__main__':
     all_models = ['roberta', 'roberta_small', 'visualbert', 'clip'] #, 'uniter']
@@ -12,8 +13,10 @@ if __name__ == '__main__':
         for attr in dataset_attributes[dataset]:
             for model in all_models:
                 print(f"{model} model on {dataset} dataset with attribute {attr} with encodings!!!!!!!!!!!!")
-                command = ["python", "run.py", "--dataset", f"{dataset}", "--model", f"{model}", "--attributes", f"{attr}", "--try_encoder"]
-                subprocess.run(command)
+                # command = ["python", "run.py", "--dataset", f"{dataset}", "--model", f"{model}", "--attributes", f"{attr}", "--try_encoder"]
+                command = f"python run.py --dataset {dataset} --model {model} --attributes {attr} --try_encoder"
+                # subprocess.run(command)
+                os.system(command)
 
 
     all_models = ['roberta', 'roberta_small', 'visualbert', 't5', 'lxmert', 'clip'] # , 'uniter']
@@ -22,5 +25,7 @@ if __name__ == '__main__':
         for dataset in all_datasets:
             for attr in dataset_attributes[dataset]:
                 print(f"{model} model on {dataset} dataset with attribute {attr} with embeddings!!!!!!!!!!!!")
-                command = ["python", "run.py", "--dataset", f"{dataset}", "--model", f"{model}", "--attributes", f"{attr}"]
-                subprocess.run(command)
+                # command = ["python", "run.py", "--dataset", f"{dataset}", "--model", f"{model}", "--attributes", f"{attr}"]
+                command = f"python run.py --dataset {dataset} --model {model} --attributes {attr}"
+                # subprocess.run(command)
+                os.system(command)
